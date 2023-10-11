9800fc8f095934366f4ef1852165a97e4f1c9eef
Abanoub Eshak Azmy <127612421+AbanoubEshak@users.noreply.github.com>
2023-10-11 15:18:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("btn").onclick= function () {
+    alert("تم نسخ النص")
+}
+document.getElementById("bttn").onclick= function () {
+    alert("تم نسخ النص")
+}
+document.getElementById("btttn").onclick= function () {
+    alert("تم نسخ النص")
+}
